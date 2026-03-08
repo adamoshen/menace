7f2dcd6c78dc99310ff095c987bb49c3e711da7d
Adam Shen <46703463+adamoshen@users.noreply.github.com>
2026-03-08 01:13:23 -0500

    :sparkles: Create `shout()`

---
+++ R/shout.R
@@ -0,0 +1,29 @@
+#' Return a character vector loudly
+#'
+#' Return a character vector loudly by uppercasing all letters and increasing spacing. Best used
+#' in conjunction with [teams_bold()].
+#'
+#' Inspired by Ryan Kavanagh.
+#'
+#' @param string A character vector.
+#' @returns A character vector with the same length as the input.
+#' @examples
+#' # example code
+#' msg <- "look here, you little shit"
+#' shout(msg)
+#'
+#' # Recommended to use with `teams_bold()`.
+#' # Write to clipboard and paste to Teams
+#' # msg |>
+#' #   shout() |>
+#' #   teams_bold() |>
+#' #   clipr::write_clip()
+#' @export
+shout <- function(string) {
+  string %>%
+    purrr::map_chr(stringr::str_to_upper) %>%
+    purrr::map_chr(\(s) stringr::str_replace_all(s, " ", "   ")) %>%
+    purrr::map_chr(\(s) stringr::str_replace_all(s, "\\B|\\b", " ")) %>%
+    purrr::map_chr(\(s) stringr::str_replace_all(s, " {5,}", "    ")) %>%
+    purrr::map_chr(stringr::str_trim)
+}
